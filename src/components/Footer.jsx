98085f77e7f58bf6f015344abcce16a4eafb4f20
qb1968 <mark.allison75@gmail.com>
2025-07-29 23:11:19 -0400

    social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,41 @@ function Footer() {
     <footer className="bg-black text-white py-8">
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
         <div className="text-sm text-gray-400 font-bold">
-          © {new Date().getFullYear()} <Link to="https://allwebcon.com" target="_blank" className="underline font-extrabold text-white">Allison Web Consultants</Link>. All rights
-          reserved.
+          © {new Date().getFullYear()}{" "}
+          <Link
+            to="https://allwebcon.com"
+            target="_blank"
+            className="underline font-extrabold text-white"
+          >
+            Allison Web Consultants
+          </Link>
+          . All rights reserved.
         </div>
 
         <div className="flex gap-4 text-xl">
           <a
-            href="https://facebook.com"
+            href="https://www.facebook.com/heather.harlow.18/"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-pink-500 transition"
+            className="hover:text-red-700 transition"
           >
             <FaFacebookF />
           </a>
           <a
-            href="https://instagram.com"
+            href="https://www.instagram.com/harlowtattoo/"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-pink-500 transition"
+            className="hover:text-red-700 transition"
           >
             <FaInstagram />
           </a>
           <a
-            href="https://twitter.com"
+            href="https://www.facebook.com/DivineMomentTattoo"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-pink-500 transition"
+            className="hover:text-red-700 transition"
           >
-            <FaTwitter />
-          </a>
-          <a
-            href="https://tiktok.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-pink-500 transition"
-          >
-            <FaTiktok />
+            <FaFacebookF />
           </a>
         </div>
       </div>
